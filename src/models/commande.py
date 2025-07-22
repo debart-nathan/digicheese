b4ab0ba82b120d2309a62f4907c85aae824c85b5
debart-nathan <denathan@laposte.net>
2025-07-22 17:47:49 +0200

    #27: rename and do first clean

--- src/models/commande.py
+++ src/models/commande.py
@@ -4,16 +4,12 @@ from datetime import date
 class Commande(SQLModel, table=True):
     """Table représentant les commandes passées par les clients."""
     
-    __tablename__ = "t_entcde"
+    __tablename__ = "t_commande"
     
-    commande: int | None = Field(default=None, primary_key=True)
+    commande_id: int | None = Field(default=None, primary_key=True)
     datcde: date | None = Field(default=None, nullable=True)
-    codcli: int | None = Field(default=None, foreign_key="t_client.codcli", nullable=True)
-    timbrecli: float | None = Field(default=None, nullable=True)
-    timbrecde: float | None = Field(default=None, nullable=True)
-    nbcolis: int = Field(default=1)
+    fk_client_id: int | None = Field(default=None, foreign_key="t_client.codcli", nullable=True)
+    client_timbre: float | None = Field(default=None, nullable=True)
+    commande_timbre: float | None = Field(default=None, nullable=True)
     cheqcli: float | None = Field(default=None, nullable=True)
-    idcondit: int = Field(default=0)
-    cdeComt: str | None = Field(default=None, max_length=255, nullable=True)
-    barchive: int = Field(default=0)
-    bstock: int = Field(default=0)
+    commande_commentaire: str | None = Field(default=None, max_length=255, nullable=True)
