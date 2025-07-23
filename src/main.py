cc6bc44bfad007f800cbbce1ef2903509384f64e
debart-nathan <denathan@laposte.net>
2025-07-23 15:55:51 +0200

    update import and clean code

--- src/main.py
+++ src/main.py
@@ -2,25 +2,27 @@ from fastapi import FastAPI, Depends
 from sqlmodel import SQLModel, select, Session
 
 from .database import get_db, engine
-from .models_file import (
-    Departement,
-    Commune,
-    Conditionnement,
-    Objet,
-    ObjetCond,
-    Detail,
-    DetailObjet,
-    Enseigne,
-    Poids,
-    Role,
-    Utilisateur,
-    RoleUtilisateur
+
+from .routers import (
+    router_commande,
+    router_client,
+    router_colis,
+    router_commune,
+    router_detail_colis,
+    router_departement,
+    router_detail_commande
 )
 
-from .routers import router_commande
-
 app = FastAPI()
+
 app.include_router(router_commande)
+app.include_router(router_client)
+app.include_router(router_colis)
+app.include_router(router_commune)
+app.include_router(router_detail_colis)
+app.include_router(router_departement)
+app.include_router(router_detail_commande)
+
 SQLModel.metadata.create_all(bind=engine)
 
 @app.get("/")
--- src/models/commune_model.py
+++ src/models/commune_model.py
@@ -7,7 +7,7 @@ class Commune(SQLModel, table=True):
     __tablename__ = "t_communes"
     
     commune_id: int | None = Field(default=None, primary_key=True)
-    fk_commune_departement: str = Field(foreign_key="t_departement.code_departement", max_length=2, nullable=False)
+    fk_commune_departement: str = Field(foreign_key="t_departements.departement_code", max_length=2, nullable=False)
     commune_codepostal: str | None = Field(default=None, max_length=5, nullable=True)
     commune_ville: str | None = Field(default=None, max_length=50, nullable=True)
     
