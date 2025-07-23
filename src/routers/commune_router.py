2aaa71e299b394428bff499e302e1c6a41a8ade7
Bluwen <blougd@gmail.com>
2025-07-23 14:52:01 +0200

    #9 #10 #11 #13 #14 #16 #18 #20 #21 #23

---
+++ src/routers/commune_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from ..database import get_db
+from ..models import Commune
+
+router = APIRouter(prefix="/commune",tags=['Commune'])
+
+@router.get("")
+def get_all_communes(session: Session= Depends(get_db))->list[Commune]:
+    return []
+
+@router.get("/{id}" )
+def get_commune(id:int,session: Session= Depends(get_db)):
+    pass
+
+@router.post("")
+def post_commune(session: Session= Depends(get_db)):
+    pass
+
+
+@router.patch("/{id}")
+def patch_commune(id:int,session: Session= Depends(get_db)):
+    pass
+
+
+@router.delete("/{id}")
+def delete_commune(id:int,session: Session= Depends(get_db)):
+    pass
---
+++ src/routers/departement_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+from ..database import get_db
+from ..models import Departement
+
+router = APIRouter(prefix="/departement",tags=['Departement'])
+
+@router.get("")
+def get_all_departements(session: Session= Depends(get_db))->list[Departement]:
+    return []
+
+@router.get("/{id}" )
+def get_departement(id:int,session: Session= Depends(get_db)):
+    pass
+
+@router.post("")
+def post_departement(session: Session= Depends(get_db)):
+    pass
+
+
+@router.patch("/{id}")
+def patch_departement(id:int,session: Session= Depends(get_db)):
+    pass
+
+
+@router.delete("/{id}")
+def delete_departement(id:int,session: Session= Depends(get_db)):
+    pass
