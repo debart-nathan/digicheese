589a396dfc24acac6e8a6d4bf371d41e7a6f3d81
Bluwen <blougd@gmail.com>
2025-07-23 17:12:15 +0200

    add import in init for model client

--- src/routers/client_router.py
+++ src/routers/client_router.py
@@ -1,28 +1,28 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 from ..database import get_db
-from ..models import Client
+from ..models import Client, ClientCreate, ClientUpdate, ClientRead
 
 router = APIRouter(prefix="/client",tags=['Client'])
 
 @router.get("/")
-def get_all_clients(client: Client, session: Session= Depends(get_db))->list[Client]:
+def get_all_clients(client: ClientRead, session: Session= Depends(get_db))->list[ClientRead]:
     return []
 
 @router.get("/{id}" )
-def get_client(id:int, client: Client, session: Session= Depends(get_db)):
+def get_client(id:int, client: ClientRead, session: Session= Depends(get_db)):
     pass
 
 @router.post("/")
-def post_client(client: Client, session: Session= Depends(get_db)):
+def post_client(client: ClientCreate, session: Session= Depends(get_db)):
     pass
 
 
 @router.patch("/{id}")
-def patch_client(id:int, client: Client, session: Session= Depends(get_db)):
+def patch_client(id:int, client: ClientUpdate, session: Session= Depends(get_db)):
     pass
 
 
 @router.delete("/{id}")
-def delete_client(id:int, client: Client, session: Session= Depends(get_db)):
+def delete_client(id:int, session: Session= Depends(get_db)):
     pass
