20c47d78aa4240c4c77de4ae2a5e24e0f1d8c23d
debart-nathan <denathan@laposte.net>
2025-07-23 12:31:13 +0200

    #27: fix foreign key in commande toward client

--- src/models/commande_model.py
+++ src/models/commande_model.py
@@ -4,11 +4,11 @@ from datetime import date
 class Commande(SQLModel, table=True):
     """Table représentant les commandes passées par les clients."""
     
-    __tablename__ = "t_commande"
+    __tablename__ = "t_commandes"
     
     commande_id: int | None = Field(default=None, primary_key=True)
     commande_date: date | None = Field(default=None, nullable=True)
-    fk_client_id: int | None = Field(default=None, foreign_key="t_client.codcli", nullable=True)
+    fk_client_id: int | None = Field(default=None, foreign_key="t_clients.client_id", nullable=True)
     client_timbre: float | None = Field(default=None, nullable=True) #value of the timbre send by the client
     commande_timbre: float | None = Field(default=None, nullable=True) #value of the timbre needed for the order
     client_cheque: float | None = Field(default=None, nullable=True) #value of the check send by the client
