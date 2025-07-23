165c55bec8579e5b30ff8d7250cb38fe6ea6115d
debart-nathan <denathan@laposte.net>
2025-07-23 09:25:53 +0200

    #27: rename column using naming convention

--- src/models/commande.py
+++ src/models/commande.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field
 from datetime import date
 
 class Commande(SQLModel, table=True):
@@ -7,9 +7,9 @@ class Commande(SQLModel, table=True):
     __tablename__ = "t_commande"
     
     commande_id: int | None = Field(default=None, primary_key=True)
-    datcde: date | None = Field(default=None, nullable=True)
+    commande_date: date | None = Field(default=None, nullable=True)
     fk_client_id: int | None = Field(default=None, foreign_key="t_client.codcli", nullable=True)
     client_timbre: float | None = Field(default=None, nullable=True)
     commande_timbre: float | None = Field(default=None, nullable=True)
-    cheqcli: float | None = Field(default=None, nullable=True)
+    client_cheque: float | None = Field(default=None, nullable=True)
     commande_commentaire: str | None = Field(default=None, max_length=255, nullable=True)
