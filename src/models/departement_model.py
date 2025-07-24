4f8acc0b2563714e6882a11e9a38f2afd24a9a50
Bluwen <blougd@gmail.com>
2025-07-24 15:42:30 +0200

    Modification de Liste dans departement model

--- src/models/departement_model.py
+++ src/models/departement_model.py
@@ -1,11 +1,8 @@
 from sqlmodel import SQLModel, Field, Relationship
-from typing import List
 
 class DepartementBase(SQLModel):
     departement_nom: str | None = Field(default=None, max_length=50, nullable=True)
     
-
-
 class Departement(DepartementBase, table=True):
     """Table représentant les départements français."""
     
@@ -13,8 +10,7 @@ class Departement(DepartementBase, table=True):
     
     departement_code: str | None = Field(primary_key=True, max_length=2)
 
-    communes: List["Commune"] = Relationship(back_populates="departement")
-
+    communes: list["Commune"] = Relationship(back_populates="departement")
 
 class DepartementCreate(DepartementBase):
     pass  # héritage direct, tous champs requis
