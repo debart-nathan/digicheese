2b2abaa4b6803203697e38392c86d98d7a5e19ae
debart-nathan <denathan@laposte.net>
2025-07-24 11:11:43 +0200

    #49 #45 : add init for schema colis and detail colis

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,5 +3,5 @@ from .detail_commande_model import DetailCommande
 from .client_model import Client
 from .commune_model import Commune
 from .departement_model import Departement
-from .colis_model import Colis
-from .detail_colis_model import DetailColis
+from .colis_model import Colis , ColisCreate, ColisRead, ColisUpdate
+from .detail_colis_model import DetailColis, DetailColisCreate , DetailColisRead, DetailColisUpdate
