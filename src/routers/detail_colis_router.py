b626da91b7038d4490476a1d1dc51fa4a9d414b7
debart-nathan <denathan@laposte.net>
2025-07-25 08:32:17 +0200

    #59 link repository to rooter colis

--- src/routers/detail_colis_router.py
+++ src/routers/detail_colis_router.py
@@ -1,28 +1,32 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlmodel import Session
 from ..database import get_db
 from ..models import DetailColis, DetailColisCreate, DetailColisRead, DetailColisUpdate
+from ..repositories import DetailColisRepository
 
-router = APIRouter(prefix="/detail-colis",tags=['DetailColis'])
+router = APIRouter(prefix="/detail_colis",tags=['DetailColis'])
 
 @router.get("/", response_model=list[DetailColisRead])
-def get_all_detail_colis(session: Session= Depends(get_db))->list[DetailColis]:
-    return []
+def get_all_detail_colis(offset: int = 0, limit: int = Query(default=100, le=100),session: Session= Depends(get_db))->list[DetailColis]:
+    return DetailColisRepository(session).get_all_detail_colis(limit,offset)
 
 @router.get("/{id}", response_model=DetailColisRead )
 def get_detail_colis(id:int,session: Session= Depends(get_db)):
-    pass
+    return DetailColisRepository(session).get_detail_colis(id)
 
-@router.post("/")
-def post_detail_colis(detail_colis:DetailColisCreate,session: Session= Depends(get_db)):
-    pass
+@router.post("/", response_model=DetailColisRead)
+def post_detail_colis(detail_colis: DetailColisCreate, session: Session = Depends(get_db)):
+    detail_colis_instance = DetailColis.model_validate(detail_colis)
+    created_detail_colis = DetailColisRepository(session).create_detail_colis(detail_colis_instance)
+    return created_detail_colis
 
 
 @router.patch("/{id}")
 def patch_detail_colis(id:int,detail_colis:DetailColisUpdate,session: Session= Depends(get_db)):
-    pass
+    created_detail_colis = DetailColisRepository(session).update_detail_colis(id,**detail_colis.model_dump(exclude_unset=True))
+    return created_detail_colis
 
 
 @router.delete("/{id}")
 def delete_detail_colis(id:int,session: Session= Depends(get_db)):
-    pass
+    return DetailColisRepository(session).delete_detail_colis(id)
