8275eb07bea7d94ad228a9235fc8655703a4ce1c
debart-nathan <denathan@laposte.net>
2025-07-25 15:14:58 +0200

    #85: Add HTTP Code Detail_Commande

--- src/routers/detail_commande_router.py
+++ src/routers/detail_commande_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException, status
 from sqlmodel import Session
 from ..database import get_db
 from ..models import DetailCommande, DetailCommandeCreate, DetailCommandeRead, DetailCommandeUpdate
@@ -12,21 +12,29 @@ def get_all_detail_commande(offset: int = 0, limit: int = Query(default=100, le=
 
 @router.get("/{id}", response_model=DetailCommandeRead )
 def get_detail_commande(id:int,session: Session= Depends(get_db)):
-    return DetailCommandeRepository(session).get_detail_commande(id)
+    detail_commande= DetailCommandeRepository(session).get_detail_commande(id)
+    if not detail_commande:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Commande: {id} non trouvé")
+    return detail_commande
 
-@router.post("/", response_model=DetailCommandeRead)
+@router.post("/", response_model=DetailCommandeRead, status_code=status.HTTP_201_CREATED)
 def post_detail_commande(detail_commande: DetailCommandeCreate, session: Session = Depends(get_db)):
     detail_commande_instance = DetailCommande.model_validate(detail_commande)
     created_detail_commande = DetailCommandeRepository(session).create_detail_commande(detail_commande_instance)
     return created_detail_commande
 
 
-@router.patch("/{id}")
+@router.patch("/{id}",response_model=DetailCommandeRead)
 def patch_detail_commande(id:int,detail_commande:DetailCommandeUpdate,session: Session= Depends(get_db)):
     created_detail_commande = DetailCommandeRepository(session).update_detail_commande(id,**detail_commande.model_dump(exclude_unset=True))
+    if not created_detail_commande:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Commande: {id} non trouvé")
     return created_detail_commande
 
 
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_detail_commande(id:int,session: Session= Depends(get_db)):
-    return DetailCommandeRepository(session).delete_detail_commande(id)
+    detail_commande= DetailCommandeRepository(session).delete_detail_commande(id)
+    if not detail_commande:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Commande: {id} non trouvé")
+    return {"ok" : True}
