7bcfcbe047472b5b60c4c97d67220f9658d052ac
debart-nathan <denathan@laposte.net>
2025-07-25 15:06:09 +0200

    #84 : Add HTTP Code Detail_colis

--- src/routers/detail_colis_router.py
+++ src/routers/detail_colis_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException, status
 from sqlmodel import Session
 from ..database import get_db
 from ..models import DetailColis, DetailColisCreate, DetailColisRead, DetailColisUpdate
@@ -12,9 +12,12 @@ def get_all_detail_colis(offset: int = 0, limit: int = Query(default=100, le=100
 
 @router.get("/{id}", response_model=DetailColisRead )
 def get_detail_colis(id:int,session: Session= Depends(get_db)):
-    return DetailColisRepository(session).get_detail_colis(id)
+    detail_colis= DetailColisRepository(session).get_detail_colis(id)
+    if not detail_colis:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Colis: {id} non trouvé")
+    return detail_colis
 
-@router.post("/", response_model=DetailColisRead)
+@router.post("/", response_model=DetailColisRead,status_code=status.HTTP_201_CREATED)
 def post_detail_colis(detail_colis: DetailColisCreate, session: Session = Depends(get_db)):
     detail_colis_instance = DetailColis.model_validate(detail_colis)
     created_detail_colis = DetailColisRepository(session).create_detail_colis(detail_colis_instance)
@@ -24,9 +27,14 @@ def post_detail_colis(detail_colis: DetailColisCreate, session: Session = Depend
 @router.patch("/{id}")
 def patch_detail_colis(id:int,detail_colis:DetailColisUpdate,session: Session= Depends(get_db)):
     created_detail_colis = DetailColisRepository(session).update_detail_colis(id,**detail_colis.model_dump(exclude_unset=True))
+    if not created_detail_colis:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Colis: {id} non trouvé")
     return created_detail_colis
 
 
-@router.delete("/{id}")
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_detail_colis(id:int,session: Session= Depends(get_db)):
-    return DetailColisRepository(session).delete_detail_colis(id)
+    result: bool = DetailColisRepository(session).delete_detail_colis(id)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Detail Colis: {id} non trouvé")
+    return {"ok": True}
